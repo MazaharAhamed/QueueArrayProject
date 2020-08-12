ba8eeccbcf733e3f5456f107097ebe2eb9fd34c0
MazaharAhamed <mazaharahamed77@gmail.com>
2020-08-12 21:28:20 +0530

    Add project files.

---
+++ QueueArrayProject/Demo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueArrayProject
+{
+    class Demo
+    {
+        static void Main(String[] args)
+        {
+            int choice, x;
+
+            QueueA quA = new QueueA(8);
+
+            while(true)
+            {
+                Console.WriteLine("1.Insert an element in Queue");
+                Console.WriteLine("2.Delete an element from Queue");
+                Console.WriteLine("3.Display front element");
+                Console.WriteLine("4.Display all elements of Queue");
+                Console.WriteLine("5.Display Size of Queue");
+                Console.WriteLine("6.Quit");
+                Console.WriteLine("Enter your choice:");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 6)
+                    break;
+                
+                switch(choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the element to be inserted:");
+                        x= Convert.ToInt32(Console.ReadLine());
+                        quA.Insert(x);
+                        break;
+                    case 2:
+                        Console.WriteLine("Deleted element is: " + quA.Delete());
+                        break;
+                    case 3:
+                        Console.WriteLine("Front Element is: " + quA.Peek());
+                        break;
+                    case 4:
+                        quA.Display();
+                        break;
+                    case 5:
+                        Console.WriteLine("Size of Queue is: " + quA.Size());
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice:");
+                        break;
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ QueueArrayProject/QueueA.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Xml;
+
+namespace QueueArrayProject
+{
+    class QueueA
+    {
+        private int[] QueueArray;
+        private int front=-1;
+        private int rear = -1;
+
+        public QueueA()
+        {
+            QueueArray = new int[10];
+            front = -1;
+            rear = -1;
+        }
+
+        public QueueA(int maxSize)
+        {
+            QueueArray = new int[maxSize];
+            front = -1;
+            rear = -1;
+        }
+
+        public bool isEmpty()
+        {
+            return (front == -1 || front == rear+1);
+        }
+
+        public bool isFull()
+        {
+            return (rear == QueueArray.Length - 1);
+        }
+
+        public int Size()
+        {
+            if (isEmpty())
+                return 0;
+            else
+                return rear - front + 1;
+        }
+
+        public void Insert(int x)
+        {
+            if (isFull())
+            {
+                Console.WriteLine("Queue Overflow");
+                return;
+            }
+            if (front == -1)
+                front = 0;
+            rear = rear + 1;
+            QueueArray[rear] = x;
+        }
+
+        public int Delete()
+        {
+            int x;
+            if (isEmpty())
+                throw new System.InvalidOperationException("Queue Underflow");
+            x = QueueArray[front];
+            front = front + 1;
+            return x;
+        }
+
+        public int Peek()
+        {
+            if (isEmpty())
+                throw new System.InvalidOperationException("Queue Underflow");
+            return QueueArray[front];
+        }
+
+        public void Display()
+        {
+            if (isEmpty())
+                Console.WriteLine("Queue is Empty");
+            Console.WriteLine("Queue is:");
+            for(int i=front;i<=rear;i++)
+            {
+                Console.WriteLine(QueueArray[i] + " ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
